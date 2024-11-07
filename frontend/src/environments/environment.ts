a1ec5aa3fd9dd4fccf9517e08f053e0e9727ea0b
wanderlee <wanderleedev@gmail.com>
2024-11-06 23:41:11 -0500

    refactor path

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,4 @@
 export const environment = {
   GEMINI_API: import.meta.env.NG_GEMINI_API,
-  MINLENGTH_INPUT: import.meta.env.NG_MINLENGTH_INPUT,
-  PATTERN_INPUT: import.meta.env.NG_PATTERN_INPUT,
   KEY_TASKS_STORAGE: import.meta.env.NG_KEY_TASKS_STORAGE,
 };
